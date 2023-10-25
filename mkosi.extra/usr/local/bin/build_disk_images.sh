b734ed6067d36a7c52268617da011a899fd72362
Vasudevan Perumal <vasuper@amazon.com>
2023-10-25 13:43:31 -0500

    testing home sparse persistent mount units

--- mkosi.extra/usr/local/bin/build_disk_images.sh
+++ mkosi.extra/usr/local/bin/build_disk_images.sh
@@ -1,25 +1,33 @@
-#!/bin/bash
+#/usr/bin/bash
 
-MOUNT_DIR
+set -eEuo pipefail
+
+if [ "$(id -u)" != 0 ]; then
+    echo "This script must be run as root!"
+    exit 1
+fi
+
+DATA_STORE="/data_images"
 
 function setup_data_images() {
     echo "==> Building data images"
-    mkdir -p /data_images
     declare -a images=("home" "var")
-    for image in "${images[@]}"; do
-        truncate -s 512MB "/data_images/$image.img"
-        mkfs.ext4 -F "/data_images/$image.img"
-    done
-    echo "==> Create sparse disk data images"
+    if [ -d "$DATA_STORE" ]; then
+	for image in "${images[@]}"; do
+	    if [ -e "$image.img"]; then
+		echo "Sparse disk file for $image already exists in $DATA_STORE"
+	    else
+		echo "==> Creating sparse disks file for $image"
+		dd if=/dev/zero of=$DATA_STORE/$image.img bs=1 count=0 seek=512M
+		mkfs.ext4 -F "$DATA_STORE/$image.img"
+	    fi
+	done
+    else
+    	echo "$DATA_STORE is not present, please check the boot chain or for failed mount units in systemd"
+    fi
 }
-
-function mount_data_part() {
-	mount /dev/disk/by-label/presistent_part /dev
-}
-
 
 function main() {
-	mount_data_part
 	setup_data_images
 }
 
