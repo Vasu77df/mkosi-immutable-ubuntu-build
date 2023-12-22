a533b121f73f9c66160f143f2d61f8f7d0ce8292
vasuper <vasu3797@gmail.com>
2023-12-22 08:51:55 -0600

    latest wrapper changes

--- build_image.py
+++ build_image.py
@@ -8,7 +8,9 @@ from contextlib import contextmanager
 parser = argparse.ArgumentParser(description="A simple docker wrapper to run mkosi in a docker container", usage="whaled-mkosi --build")
 parser.add_argument("-d", "--debug", actions="store_true", help="Verbose Logging")
 parser.add_argument("--build", actions="store_true", help="Build mkosi-builder container and run the image build")
-parser.add_argument
+parser.add_argument("--run")
+
+
 @contextmanager
 def step_wrapper(stepname: str):
     print(f'Running {stepname}')
@@ -23,7 +25,11 @@ def main():
     with step_wrapper('Listing Docker Cont'):
         print('steps')
 
+
 try:
     main()
 except Exception as e:
     print('we gotta exception houston', e)
+
+
+## Docker command to incorporate
